10557ad9f05fc3689268095272c5be9c90e14d32
Sargis Sargsyan <sargissargsyan@workfront.com>
2017-12-21 20:21:55 +0400

    Google search final

--- src/test/java/GoogleSearchTest.java
+++ src/test/java/GoogleSearchTest.java
@@ -3,6 +3,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertTrue;
+
 /**
  * Created by sargis on 12/18/17
  */
@@ -20,12 +22,14 @@ public class GoogleSearchTest {
 
     @AfterMethod
     public void tearDown() {
-
+        driver.close();
     }
 
     @Test
     public void searchTest() {
         googleSearchPage.search("Armenia");
+        assertTrue(googleSearchPage.getFirstResult().getText().contains("Wikipedia"));
+
 
 
     }
