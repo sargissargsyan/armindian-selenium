c5f8d8e107a6f6c06f3a18fd17191976b469e1fb
Sargis Sargsyan <sargissargsyan@workfront.com>
2017-12-25 21:19:10 +0400

    clean up

--- src/main/java/WindowsPage.java
+++ src/main/java/WindowsPage.java
@@ -1,7 +1,4 @@
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 
 /**
  * Created by sargis on 12/25/17
@@ -22,28 +19,5 @@ public class WindowsPage extends BasePage {
 
     public void switchToWindow(int index) {
         driver.switchTo().window(getAllWindows()[index].toString());
-    }
-
-    public void hoverElement(WebElement element){
-        Actions actions = new Actions(driver);
-        actions.moveToElement(element).build().perform();
-    }
-
-    public void hoverAvatar() {
-        hoverElement(find(By.className("figure")));
-
-    }
-
-    public boolean isHeaderDisplayed() {
-        return isDisplayed(By.className("figcaption"));
-    }
-
-    public boolean isHeaderNotDisplayed() {
-        return isNotDisplayed(find(By.className("figcaption")), 5);
-    }
-
-    public WebElement getHeader() {
-        hoverAvatar();
-        return find(By.className("figcaption"));
     }
 }
