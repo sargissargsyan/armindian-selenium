12d5817aff65be739e7f84bac19bbf04607f3ba2
Sargis Sargsyan <sargissargsyan@workfront.com>
2017-12-25 19:59:51 +0400

    dropdown select

---
+++ src/main/java/DropdownPage.java
@@ -0,0 +1,42 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+/**
+ * Created by sargis on 12/25/17
+ */
+public class DropdownPage extends BasePage {
+    public DropdownPage(WebDriver webDriver) {
+        super(webDriver);
+        visit("http://the-internet.herokuapp.com/dropdown");
+    }
+
+    public WebElement getDropdown() {
+        return find(By.id("dropdown"));
+    }
+
+    public List<WebElement> getDropdownOptions() {
+        return getDropdown().findElements(By.tagName("option"));
+    }
+
+    public void selectOption(String optionName) {
+        List<WebElement> options = getDropdownOptions();
+        for (WebElement option:options) {
+            if (option.getText().equals(optionName)) {
+                option.click();
+            }
+        }
+    }
+
+    public String getSelectedOption() {
+        List<WebElement> options = getDropdownOptions();
+        for (WebElement option:options) {
+            if (option.isSelected()) {
+                return option.getText();
+            }
+        }
+        return null;
+    }
+}
---
+++ src/test/java/DropdownTest.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * Created by sargis on 12/21/17.
+ */
+public class DropdownTest {
+    private ChromeDriver driver;
+    private DropdownPage dropdownPage;
+
+    @BeforeMethod
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "/Users/sargis/dev/chromedriver/chromedriver");
+        driver = new ChromeDriver();
+        dropdownPage = new DropdownPage(driver);
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.close();
+    }
+
+    @Test
+    public void dropdownSelect() {
+        dropdownPage.selectOption("Option 2");
+        assertEquals(dropdownPage.getSelectedOption(), "Option 2");
+    }
+
+}
